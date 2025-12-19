c03b094b0b2e7cc9695921cee43b3bfce71bb391
WuTing201y <wuting.work@gmail.com>
2025-12-19 16:22:16 +0800

    Update p6.ino

--- ACET/p6.ino
+++ ACET/p6.ino
@@ -1,46 +1,12 @@
 // ADP-Corvette-F1 的蜂鳴器產生振動。
 // ADP-Corvette-F1 的蜂鳴器產生音樂及唱歌。
-
 int speakerPin = D3;
 
-void setup(){
-  pinMode(speakerPin, OUTPUT);
-  // 也可以直接寫成 pinMode(D3, OUTPUT);
-  digitalWrite(D3, HIGH);
-}
-// do re mi fa so la si 
-// c  d  e  f  g   a  b
-char song[] = "ccggaagffeeddc "; //空白=>休止符
-unsigned long beats[] = {1,1,1,1,1,1,2,1,1,1,1,1,1,2,4};
-int tempo = 300;
-int tones[] = {261, 294, 330, 349, 392, 440, 494};
-
-void loop()
-{
-  for(int i = 0 ; i < sizeof(song); i++)
-  {
-    if(song[i] = ' ') delay(tempo * beats[i]);
-    else
-    {
-      tone(D3, tones[i], tempo * beats[i]);
-      delay(tempo);
-      noTone(D3);
-    }
-    
-  }
-  delay(tempo);
-}
-//==================================================
-int speakerPin = D3;
-
-// 依照簡譜的順序,填入代表的音符,空白代表休止符
 char notes[] = "ccggaagffeeddc ";
-
 unsigned long beats[] = {1,1,1,1,1,1,2,1,1,1,1,1,1,2,4};
-// 利用 sizeof(),算出總共要多少音符
 int length = sizeof(notes);
-// 決定一拍多長,這邊一拍 300 ms
 int tempo = 300;
+
 void setup() {
 pinMode(speakerPin, OUTPUT);
 }
@@ -69,4 +35,37 @@ void playNote(int OutputPin, char note, unsigned long duration) {
       noTone(OutputPin);
     }
   }
+}
+
+//=============================
+void setup(){
+	pinMode(D3, OUTPUT);
+}
+
+char song[] = "ccggaagffeeddc ";
+unsigned long beats[] = {1,1,1,1,1,1,2, 1,1,1,1,1,1,2, 4};
+int len = sizeof(song);
+int tmp = 300;
+
+void loop(){
+	for(int i = 0; i < len; i++){
+		if(song[i] == ' '){
+			delay(tmp * beats[i]);
+		}else{
+			playfunc(song[i], tmp*beats[i]);
+		}
+		delay(tmp/10);
+	}
+}
+
+void playfunc(char note, unsigned long dur){
+	char notes[] = {'c','d','e','f','g','a','b','C'};
+	int tones[] = {261, 294, 330, 349, 392, 440, 494, 523};
+	for(int i = 0; i < 8; i++){
+		if(notes[i] == note){
+			tone(D3, tones[i], dur);
+			delay(dur);
+			noTone(D3);
+		}
+	}
 }
