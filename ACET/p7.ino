ed41cd829aecd19cd27d99ff5723e1ca287baccb
WuTing201y <wuting.work@gmail.com>
2025-12-19 15:29:04 +0800

    Update p7.ino

--- ACET/p7.ino
+++ ACET/p7.ino
@@ -5,30 +5,30 @@ ADP-Corvette-F1 的 COM 上傳送數字1~9,UART 顯示:數字 1~9。
 int num = 65;
 int i = 1;
 char ch;
-int num;
+
 
 void setup(){
-	Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 
 void loop()
 {
-	Serial.print(num);
-	delay(500);
+  Serial.print(num);
+  delay(500);
 
-	if(Serial.avaliavle()){
-		ch = Serial.read();
-		if(ch > '0' && ch <= '9'){
-			num = ch - '0';
-			Serial.print(num);
-			Serial.print(" ");
-			for(int j = 0; j < num; j++){
-				Serial.print("*")
-			}
-			Serial.println();
-		}
-	}
+  if(Serial.available()){
+    ch = Serial.read();
+    if(ch > '0' && ch <= '9'){
+      num = ch - '0';
+      Serial.print(num);
+      Serial.print(" ");
+      for(int j = 0; j < num; j++){
+        Serial.print("*");
+      }
+      Serial.println();
+    }
+  }
 
 
 }
