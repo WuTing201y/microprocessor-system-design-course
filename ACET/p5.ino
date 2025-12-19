09e7931f13e7966742dc20f7d3ff6e40fe412785
WuTing201y <wuting.work@gmail.com>
2025-12-19 15:11:58 +0800

    done

--- ACET/p5.ino
+++ ACET/p5.ino
@@ -2,41 +2,47 @@
 LiquidCrystal lcd(8,9,4,5,6,7);
 #include<RTC.h>
 
-byte year = 2020;
+int year = 2020;
 byte month = 12;
 byte day = 25;
 byte hour = 12;
-byte minute = 20
+byte minute = 20;
 byte second = 3;
 
 void setup(){
-	lcd.begin(16,2);
-	lcd.clear();
-	RTC.settime(day, hour, minute, second);
-	RTC.startcounting();
+  lcd.begin(16,2);
+  lcd.clear();
+  RTC.settime(day, hour, minute, second);
+  RTC.startcounting();
 }
 
 void print2(byte value)
 {
-	if(value<10){
-		lcd.print("0");
-	}
-	lcd.print(value);
+  if(value<10){
+    lcd.print("0");
+  }
+  lcd.print(value);
 }
 
 void loop()
 {
-	lcd.setCursor(0,0);
-	lcd.print(year);
-	lcd.print("/");
-	lcd.print(print2(month));
-	lcd.print("/");
-	lcd.print(print2(RTC.getday()));
-	lcd.print("");
-
-	lcd.print(print2(RTC.gethour()));
-	lcd.print(":");
-	lcd.print(print2(RTC.getminutes()));
-	lcd.print(":");
-	lcd.print(print2(RTC.getseconds()));
+  lcd.setCursor(0,0);
+  
+  byte dd = RTC.getday();
+  byte hr = RTC.gethours();
+  byte mins = RTC.getminutes();
+  byte sec = RTC.getseconds();
+  
+  lcd.print(year);
+  lcd.print("/");
+  print2(month);  // print2函式已經有print了，不需要再寫lcd.print(print2(...))
+  lcd.print("/");
+  print2(dd);
+  
+  lcd.setCursor(0,1);
+  print2(hr);
+  lcd.print(":");
+  print2(mins);
+  lcd.print(":");
+  print2(sec);
 }
